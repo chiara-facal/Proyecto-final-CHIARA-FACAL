69196f6d5e74b968453249b779526acb2ace359e
chiara-facal <chiarafacal@gmail.com>
2023-03-22 19:50:03 -0300

    Creacion del proyecto y primer modelo

---
+++ ProyectoFinal/AlumnosCoder/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class EntradaDeBlog(models.Model):
+    titulo_entrada = models.CharField(max_length=50)
+    subtitulo_entrada= models.CharField (max_length = 30)
+    descripcion = models.CharField (max_length = 200)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f' {self.titulo_entrada - {self.fecha_creacion}} '
